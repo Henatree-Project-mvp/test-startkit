a60928f05bf29eea74c09711ee2b7d3525081a9b
MATHIEU STROSBERG <amthieu@mathieu-strosberg.home>
2021-05-22 17:39:21 +0200

    add logo and edit title in settingsScreen

--- containers/SettingsScreen.js
+++ containers/SettingsScreen.js
@@ -4,7 +4,7 @@ import { Button, Text, View } from "react-native";
 export default function SettingsScreen({ setToken }) {
   return (
     <View>
-      <Text>Hello Settings</Text>
+      <Text>Hello Settings Test Modifications</Text>
 
       <Button
         title="Log Out"
